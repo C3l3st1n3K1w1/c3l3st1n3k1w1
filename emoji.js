94b1ec00e3ffee79c05c7478819d8c3c265e2652
C3l3st1n3K1w1 <rinn2122e@gmail.com>
2026-01-13 18:00:50 -0600

    Delete emoji.jsM

--- emoji.js
+++
@@ -1,7 +0,0 @@
-// Replace every ☕ in the parent page with <emoji-coffee>
-(function () {
-  const doc = window.parent.document;
-  doc.body.innerHTML = doc.body.innerHTML.replace(/☕/g,
-    "<emoji-coffee></emoji-coffee>"
-  );
-})();
